29cc4fff8bec97d1717ea7302323b9552e8e71b7
Francisco Azevedo <a36232@alunos.ipb.pt>
2019-12-05 23:52:01 +0000

    mongodb connection feito

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
+
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@omni-bpbfv.mongodb.net/movies?retryWrites=true&w=majority', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
 
 app.use(express.json());
 app.use(routes);
