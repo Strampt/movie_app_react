fe13d57e186f2cccd97fd1364487be9e73b650c6
Francisco Azevedo <a36232@alunos.ipb.pt>
2019-12-05 22:56:49 +0000

    routes separadas do server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
+const routes = require('./routes');
 const app = express();
 
-app.get('/', (req, res) => {
-	return res.json({ message: 'Hello World' });
-});
-
+app.use(express.json());
+app.use(routes);
 app.listen(3333);
+
+/*
+27:57 2-criando a base
+yarn run dev 
+*/
