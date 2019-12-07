41be071dd7058685838efe867841a840d34c2133
Francisco Azevedo <a36232@alunos.ipb.pt>
2019-12-07 23:57:22 +0000

    add model para os filmes

---
+++ backend/src/models/Movie.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Float = require('mongoose-float').loadType(mongoose, 1);
+
+const MovieSchema = new mongoose.Schema({
+	imdb: String,
+	title: String,
+	year: Number,
+	type: String,
+	poster: String,
+	released: Date,
+	runtime: String,
+	genre: String,
+	plot: String,
+	imdbrating: Float,
+	director: String
+});
+
+module.exports = mongoose.model('Movie', MovieSchema);
