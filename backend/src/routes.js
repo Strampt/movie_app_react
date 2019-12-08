3f409f66799ed1945de6ff19b3b47ea91fe13cf2
Francisco Azevedo <a36232@alunos.ipb.pt>
2019-12-08 23:55:43 +0000

    add post route para Movie

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const UserController = require('./controllers/UserController');
+const MovieController = require('./controllers/MovieController');
 const routes = express.Router();
 
 routes.post('/sessions', UserController.store);
+
+routes.post('/movies', MovieController.store);
 
 module.exports = routes;
