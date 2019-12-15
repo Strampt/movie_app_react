faee0835a7c8b75c9c5b62d9ed74c78e8e245949
Francisco Azevedo <a36232@alunos.ipb.pt>
2019-12-15 00:09:00 +0000

    added .env file to the project

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
 
+require('dotenv').config();
+const mongoConnect = process.env.MONGO_CONNECT;
+const port = process.env.PORT;
+
 const app = express();
 // ligação para o mongodb atlas
-mongoose.connect('mongodb+srv://omnistack:omnistack@omni-bpbfv.mongodb.net/movies?retryWrites=true&w=majority', {
+mongoose.connect(mongoConnect, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
 });
 // assim o express sabe que é para usar json
 app.use(express.json());
 app.use(routes);
-app.listen(3333);
+app.listen(port);
 
 /*
 52:52 2-criando a base
